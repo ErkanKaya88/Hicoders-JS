30a919489b3907213278464f8efea029d60c23fb
ErkanKaya88 <kaya.erkan1988@gmail.com>
2022-11-01 00:20:35 +0100

    yorum satiri yazayi unutmustum. omlari ekledim

--- week-3/assigment-1/app.js
+++ week-3/assigment-1/app.js
@@ -1,6 +1,6 @@
 const sayilar = [7, 1, 1, 7, 1, 7, 99, 99, 12, 8, 12, 12, 99, 8];
 
-const sayilar2 = [];
+const sayilar2 = []; //yeni bir dizi olusturdum. elementleri icine göndermek icin
 
 for (let i = 0; i < sayilar.length; i++) {
   const element = sayilar[i];
@@ -8,11 +8,11 @@ for (let i = 0; i < sayilar.length; i++) {
   for (let j = 0; j < sayilar.length; j++) {
     if (element == sayilar[j]) {
       counter++;
-    }
+    } // sayac ve element karsilastiriliyor
   }
   if (counter === 2) {
     sayilar2.push(sayilar[i]);
-  }
+  } // sayacin 2 olmasi durumunda ayni elemandan 2 tane vardir demek
 }
 
-console.log(sayilar2[0]);
+console.log(sayilar2[0]); // olusturdugum dizi sadece önceki dizide 2 tanae bulunan element. onun da ilk elemanini yazdirdim.
